51d70469898d0eda806714380ea32e0b164d78f0
Paulo Brito <paulo.brito@visagio.com>
2024-04-06 13:40:57 -0300

    feat: add client factory

---
+++ server/database/factories/ClientFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Client;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClientFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Client::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'cpf' => $this->faker->unique()->numerify('###########'),
+            'birthday' => $this->faker->date(),
+            'gender' => $this->faker->randomElement(['M', 'F']),
+            'state' => $this->faker->stateAbbr,
+            'city' => $this->faker->city,
+            'address' => $this->faker->address,
+        ];
+    }
+}
