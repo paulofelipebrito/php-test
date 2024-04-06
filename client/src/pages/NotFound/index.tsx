8c06ae955d47a39f7769a315911c52b6128e3657
Paulo Brito <paulo.brito@visagio.com>
2024-04-06 13:37:17 -0300

    feat: add notfound route

---
+++ client/src/pages/NotFound/index.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="bg-white flex w-full h-full my-2 rounded-md items-center justify-center">
+      <div className="text-2xl">Página não encontrada</div>
+    </div>
+  );
+};
+
+export default NotFound;
