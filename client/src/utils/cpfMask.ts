593bdbe73a8331a49599421408449a40d6a053ea
Paulo Brito <paulo.brito@visagio.com>
2024-04-08 14:23:29 -0300

    fix: remove comments

--- client/src/utils/cpfMask.ts
+++ client/src/utils/cpfMask.ts
@@ -1,8 +1,8 @@
 export const applyCpfMask = (value: string) => {
-  const cpf = value.replace(/\D/g, ''); // Remove todos os caracteres que não são dígitos
-  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4'); // Aplica a máscara de CPF
+  const cpf = value.replace(/\D/g, '');
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
 };
 
 export const removeCpfMask = (value: string) => {
-  return value.replace(/\D/g, ''); // Remove all non-digit characters
+  return value.replace(/\D/g, '');
 };
